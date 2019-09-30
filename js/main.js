05655b40263dc4786ef446fc330ac7cee07d1606
Anderson Fonseca <andersongfonseca@gmail.com>
2019-09-30 07:28:50 -0300

    Exemplos de Arrays e Dicionário(objetos)

--- js/main.js
+++ js/main.js
@@ -1,6 +1,39 @@
-var nome = "Anderson Gonzaga"
-var idade = 29;
-var idade2 = 10;
-var frase = "Japão é o melhor time do mundo";
-alert(frase.replace("Japão", "Brasil"));
-console.log(frase.toLowerCase);
+// var nome = "Anderson Gonzaga"
+// var idade = 29;
+// var idade2 = 10;
+// var frase = "Japão é o melhor time do mundo";
+// alert(frase.replace("Japão", "Brasil"));
+// console.log(frase.toLowerCase());
+
+// //TRABALHANDO COM LISTAS
+// var lista = ["maça", "pera", "laranja"];
+
+// //retira ultimo elemento da lista
+// lista.pop();
+
+// //add último elemento da lista
+// lista.push("uva")
+
+// //inverte a ordem dos elementos da lista
+// lista.reverse();
+
+// //add itens em uma string
+// console.log(lista.join(" | "));
+
+
+// console.log(lista);
+
+// //retorna em uma string a lista do Array
+// console.log(lista.toString());
+
+//DICIONARIO
+
+var fruta = {
+    nome:"maça",
+    cor: "vermelha"
+}
+
+//uma lista de dicionários
+var frutas = [{nome:"maçã", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(fruta);
+alert(frutas[0].nome);
