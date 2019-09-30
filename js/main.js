108ab594891cbdbf13575a05b501a4d0ee61f154
Anderson Fonseca <andersongfonseca@gmail.com>
2019-09-30 07:03:50 -0300

    exercícios iniciais

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var nome = "Anderson Gonzaga"
+var idade = 29;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo";
+alert(frase.replace("Japão", "Brasil"));
+console.log(frase.toLowerCase);
